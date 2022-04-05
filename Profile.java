5c756f848ab5bd40e83c2f1f338cac6b64db7fb1
ErnestoCarballo <ernestowcarballo12@gmail.com>
2022-04-04 22:31:35 -0700

    initial commit

---
+++ Profile.java
@@ -0,0 +1,7 @@
+public class Profile{
+    public static void main(String[] ars){
+        System.out.println("My name is Coding Dojo");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Burbank, CA");
+    }
+    }
